961251e64a68945ef8c29642440f3a2032b617f5
Zhen Du <jenkin.du@gmail.com>
2018-09-29 12:54:46 -0400

    init

---
+++ src/main/java/com/upgrade/pacificocean/PacificOceanApplication.java
@@ -0,0 +1,12 @@
+package com.upgrade.pacificocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacificOceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacificOceanApplication.class, args);
+	}
+}
