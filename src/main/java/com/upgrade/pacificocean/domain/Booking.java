1c1ca425aa6f2133bb9c82d091d504ae4da0e610
Zhen Du <jenkin.du@gmail.com>
2018-09-30 13:28:38 -0400

    add junit

--- src/main/java/com/upgrade/pacificocean/domain/Booking.java
+++ src/main/java/com/upgrade/pacificocean/domain/Booking.java
@@ -34,4 +34,54 @@ public class Booking implements Serializable {
 	private Date check_out;
 	@XmlElement
 	private boolean cancelled;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getCamp_id() {
+		return camp_id;
+	}
+	public void setCamp_id(int camp_id) {
+		this.camp_id = camp_id;
+	}
+	public int getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(int start_date) {
+		this.start_date = start_date;
+	}
+	public int getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(int end_date) {
+		this.end_date = end_date;
+	}
+	public Date getCheck_in() {
+		return check_in;
+	}
+	public void setCheck_in(Date check_in) {
+		this.check_in = check_in;
+	}
+	public Date getCheck_out() {
+		return check_out;
+	}
+	public void setCheck_out(Date check_out) {
+		this.check_out = check_out;
+	}
+	public boolean isCancelled() {
+		return cancelled;
+	}
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+	
+	
 }
--- src/test/java/com/upgrade/pacificocean/PacificOceanApplicationTests.java
+++ src/test/java/com/upgrade/pacificocean/PacificOceanApplicationTests.java
@@ -1,16 +1,64 @@
 package com.upgrade.pacificocean;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.upgrade.pacificocean.dao.CampsiteMapper;
+import com.upgrade.pacificocean.domain.Booking;
+import com.upgrade.pacificocean.domain.Campsite;
+import com.upgrade.pacificocean.domain.Schedule;
+import com.upgrade.pacificocean.utils.Utils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PacificOceanApplicationTests {
 
+	@Autowired
+    private CampsiteMapper campsiteMapper;
+	
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void testCampsite() {
+		Campsite c = campsiteMapper.getCampsite(1);
+		Assert.assertNotNull(c);
+	}
 
+	@Test
+	public void testSchedule() {
+		int today = Utils.today();
+		List<Schedule> schedule = campsiteMapper.getSchedule(1, today);
+		Assert.assertNotNull(schedule);
+	}
+	
+	@Test
+	public void testBooking() {
+		int today = Utils.today();
+		campsiteMapper.createBooking(999, "Zhen", "zhen@du.com", 1, today, today);
+		Booking b = campsiteMapper.getBooking(999);
+		Assert.assertEquals("Zhen", b.getName());
+	}
+	
+	@Test
+	public void testUpdateBooking() {
+		int today = Utils.today();
+		campsiteMapper.updateBooking(999, "Du", "zhen@du.com", today, today, false);
+		Booking b = campsiteMapper.getBooking(999);
+		Assert.assertEquals("Du", b.getName());
+	}
+	
+	@Test
+	public void testDeleteBooking() {
+		campsiteMapper.deleteBooking(999);
+		Booking b = campsiteMapper.getBooking(999);
+		Assert.assertNull(b);
+	}
 }
